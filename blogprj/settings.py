da8b266e77193914d817b192ac16234e76336cf5
Chanran33 <k55416325@gmail.com>
2020-07-20 18:45:08 +0900

    modify setting.py

--- blogprj/settings.py
+++ blogprj/settings.py
@@ -9,10 +9,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07^dsc*sa0-hcar1nic-#k624*d&-+jn%g-i_o7)5o4becyf9c'
+# SECRET_KEY = '07^dsc*sa0-hcar1nic-#k624*d&-+jn%g-i_o7)5o4becyf9c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','07^dsc*sa0-hcar1nic-#k624*d&-+jn%g-i_o7)5o4becyf9c')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
